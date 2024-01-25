e2c5f23f3c69abd935e1da32955f83c743e93a89
alferio <ioan-cristian.alferaru@ec.europa.eu>
2024-01-25 23:33:36 +0100

    feat(kbservice)

--- kbevd.1.sh
+++ kbevd.1.sh
@@ -16,6 +16,7 @@ get_keyboard_input_devices_list() {
 service_running=true
 # Function to handle SIGINT (Ctrl+C)
 interrupt_handler() {
+  stty echo
   echo "Received Ctrl+C. Cleaning up and exiting..."
   # Add your cleanup code here if needed
   service_running=false
@@ -23,13 +24,16 @@ interrupt_handler() {
 }
 
 function main(){
-
+  min_size=1000  # Change this value to the minimum size you want to check (in bytes)
+  file_path="/tmp/kbev/events"
   # Set a trap to call the interrupt_handler function when SIGINT is received
   trap interrupt_handler SIGINT
+  trap interrupt_handler SIGTERM
 
 
   current=$(pwd)
-  mkdir -p /tmp/kbev
+  rm -rf /tmp/kbev/
+  mkdir -p /tmp/kbev/
   cd /tmp/kbev
   echo "configured events service in  : /tmp/kbev"
 
@@ -40,44 +44,48 @@ function main(){
   echo "detected input devices : $input_devices"
   echo "configuring service : "
   chmod 777 input_devices.lst
+  rm -rf events
   touch events
   chmod 777 events
   rm -rf read.lock
   ls -la
 
   # Disable echoing
-  # stty -echo
-  for device in $input_devices; do
-    # ../.././read-event $device
-    cat -u $device $device > events &
-  done
+  stty -echo
 
   #ExecStart
+  num_bytes=24
   while $service_running; do
-    pseo=$(ps -eo cmd,pid | egrep "^cat -u /dev/input/event[0-9][0-9]*")
-    IFS=$'\n'
-    ## rm -rf events
-    for process in $pseo; do
-      IFS=$'\t'
-      read -ra proc <<< "$process"
-      echo "proc ${proc[1]}, launch_cmd = ${proc[0]}"
-    done
-    sleep 20
+      if [ -f read.lock ]
+      then
+        #nothing
+        touch service.is.blocked
+        sleep 0.01
+      else
+        rm -rf service.is.blocked
+        touch read.lock
+
+        rm -rf events
+        for device in $input_devices; do
+          pp=$(ps -eo cmd | egrep "^dd if=$device")
+          if [ "$pp" == "" ];then
+            echo "launching listener for $device ---> [$pp]" > /dev/null 2>&1
+            dd if=$device bs=$num_bytes count=1 of="/tmp/kbev/event" status=none conv=notrunc oflag=append &
+          fi
+        done
+        rm -rf read.lock
+        # cat input_devices.lst
+        rm -rf events
+        echo "" > events
+      fi
   done
 
-    pseo=$(ps -eo cmd,pid | egrep "^cat -u /dev/input/event[0-9][0-9]*")
-    IFS=$'\n'
-    ## rm -rf events
-    for process in $pseo; do
-      IFS=$'\t'
-      read -ra proc <<< "$process"
-      echo "killing proc ${proc[1]}, launch_cmd = ${proc[0]}"
-      kill -SIGTERM "${proc[1]}"
-    done
   #ExecStop=
 
   # Enable echoing
-  # stty echo
+  stty echo
+  rm -rf service.is.blocked
+  rm -rf read.lock
   echo "keyboard event service exited"
   cd $current
 
