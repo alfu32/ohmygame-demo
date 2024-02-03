d19acddf1a4a21a67c66805c0c0181a1a92fab81
alfu32 <ioan.alferaru@gmail.com>
2024-02-03 01:23:59 +0100

    feat(keyboard): fixed incoming kb codes fix level rendering

--- ohmygame/level.v
+++ ohmygame/level.v
@@ -25,21 +25,12 @@ fn print_debug(s string){
 	}
 }
 pub fn (mut level Level) render(mut t Terminal, kbd Keyboard){
-	print_debug("level.current_scene.run_actions(omg.input_event_time_now(),kbd)")
 	level.current_scene.run_actions(input_event_time_now(),kbd)
-	print_debug("level.current_scene.do_collisions()")
 	level.current_scene.do_collisions()
-	/////////// print_debug("level.current_scene.remove_dead_entities()")
 	level.current_scene.remove_dead_entities()
-	print_debug("level.current_scene.update_canvas()")
 	level.current_scene.update_canvas()
-	/// print("\r${kbd.pressed.keys()}")
-	print_debug("t.clear()")
 	t.clear()
-	print_debug("level.current_scene.canvas.render_to_terminal(mut &t)")
 	level.current_scene.canvas.render_to_terminal(mut &t)
-	/// s := level.current_scene.canvas.render_to_string()
-	/// println(s)
 }
 
 pub fn (mut level Level) is_finished() bool {
